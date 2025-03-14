d2ca02a2488827c8f9d4abde28a8a8f5efc1f575
JiarongGuo <1950955951@qq.com>
2025-03-14 22:43:06 +0800

    build 2: add copyright and version

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import tkinter as tk
+import datetime as dt
 import random as rd
+import tkinter as tk
+import tkinter.messagebox as tkms
 
 # constant
-VER = (1, 0, 2, 1)
+VER = (1, 0, 2, 2)
 
 # window
 w = tk.Tk()
@@ -24,12 +26,18 @@ le = tk.Label(w, text='请输入人数')
 se = tk.Entry(w)
 bs = tk.Button(w, text='抽取', command=bsc)
 lr = tk.Label(w, textvariable=lrv)
+lv = tk.Label(w, text=f'v{VER[0]}.{VER[1]}.{VER[2]} build {VER[3]}')
+bc = tk.Button(w, text='   ', relief='flat',
+               command=lambda: tkms.showinfo('版权信息',
+                                             f'Copyright (c) {dt.date.today().year} Guo Jiarong\r\nAlright reserved.'))
 
 # placing
 le.pack()
 se.pack()
 bs.pack()
 lr.pack()
+lv.pack()
+bc.pack()
 
 # main
 if __name__ == '__main__':
